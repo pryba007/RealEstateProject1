9baf7034c6dbf5aa093914f1a4165153f63f46ee
pryba007 <piotr.ryba42@gmail.com>
2023-11-13 13:41:08 +0000

    daily save

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from'react';
+import styles from "./App.module.css";
+import catImage from "./images/kitty-cat-kitten-pet-45201.jpeg";
+
 const App = () => {
     return (
-        <div className="App">
+        <div className={styles.app}>
             <h1>Project 1</h1>
+            <p>This is the first project</p>
+            <img src={catImage} className={styles.image} alt="cat" />
         </div>
     );
 };                                                              
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-//importing the dom 
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-//adding the css
 import './index.css';
-//impprting the App component
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-//creating the root element
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//rendering the app, although the code can also be written in a functional way
-root.render(<App />); develop
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
